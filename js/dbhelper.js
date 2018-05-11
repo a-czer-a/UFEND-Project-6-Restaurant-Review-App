3c9ed8e7168f095fca21ea8dab1f0dc5455ceb88
Agata Czerwińska <agata@rubrica.pl>
2018-05-11 17:10:39 +0200

    Add script type for service_worker.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,9 @@ class DBHelper {
      */
     static get DATABASE_URL() {
         const port = 8000 // Change this to your server port
-        return `https://github.com/a-czer-a/UFEND-Project-6-Restaurant-Review-App/blob/master/data/restaurants.json`;
+
+        //        return `http://localhost:${port}/data/restaurants.json`;
+        return 'https://github.com/a-czer-a/UFEND-Project-6-Restaurant-Review-App/blob/master/data/restaurants.json';
     }
 
     /**
